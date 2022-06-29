838af1932bc6a16014ba8dd2e09860ad3d1e2571
Carmen <carmencantudo@gmail.com>
2022-06-29 19:55:04 +0000

    Change displayDivisionQuestion function to get a whole number

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "/") {
-        return [Math.floor(operand1 / operand2), "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -151,6 +151,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2
 
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
